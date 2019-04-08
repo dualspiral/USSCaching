6ac681fce5dd1ef8c445c6ea072d35e1b459c3b6
Daniel Naylor <git@drnaylor.co.uk>
2019-04-08 23:46:50 +0100

    Suitable messages.

--- src/main/java/uk/co/drnaylor/sponge/usscachingsponge/USSCachingSponge.java
+++ src/main/java/uk/co/drnaylor/sponge/usscachingsponge/USSCachingSponge.java
@@ -11,6 +11,9 @@ import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.service.user.UserStorageService;
 import org.spongepowered.common.SpongeImpl;
 
+import java.time.Duration;
+import java.time.Instant;
+
 @Plugin(
         id = "uss-caching-sponge",
         name = "USS Caching Sponge"
@@ -24,16 +27,24 @@ public class USSCachingSponge {
 
     @Listener
     public void onPreInit(GamePreInitializationEvent event) {
+        this.logger.info("Replacing UserStorageService");
         Sponge.getServiceManager()
                 .setProvider(this, UserStorageService.class, this.service);
     }
 
     @Listener
     public void onServerStart(GameStartingServerEvent event) {
-        this.logger.info("Starting init of user discoverer async");
+        this.logger.warn("-------------------------------------------------------------------------------------------");
+        this.logger.warn("You are running a testing plugin for the UserStorageService");
+        this.logger.warn("DO NOT OPEN AN ISSUE ON SPONGE IF YOU ENCOUNTER ISSUES WITH USERS WHILE THIS PLUGIN IS INSTALLED");
+        this.logger.warn("See https://github.com/SpongePowered/SpongeCommon/pull/2124");
+        this.logger.warn("-------------------------------------------------------------------------------------------");
+        this.logger.info("Starting init of user discoverer async.");
         SpongeImpl.getScheduler().createAsyncExecutor(SpongeImpl.getSpongePlugin()).execute(() -> {
+            Instant thisInstant = Instant.now();
             this.service.init();
-            this.logger.info("User discoverer init complete.");
+            Duration duration = Duration.between(thisInstant, Instant.now());
+            this.logger.info("User discoverer init complete - time taken: {} ms.", ((double) duration.toMillis()) / 1000d);
         });
     }
 }
